6dc08b647f74deece26aea34d974b356d4e2edc2
Galaxyz <97992906+Galaxyz7@users.noreply.github.com>
2024-06-08 05:47:29 -0500

    Add md editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,18 @@
-'use-client'
+'use client';
 
-import { TextField, TextArea, Button} from '@radix-ui/themes'
-import React from 'react'
+import { TextField, Button} from '@radix-ui/themes';
+import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
-function NewIssuePage() {
+
+const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root placeholder='Title'>
             <TextField.Slot />
         </TextField.Root>
-        <TextArea placeholder="Description.." />
+        <SimpleMDE placeholder='Description'/>;
         <Button>Submit New Issue</Button>
     </div>
   )
