63dc896945fbe4e48e6ecdc39a4a82d1dbf6111c
Galaxyz <97992906+Galaxyz7@users.noreply.github.com>
2024-06-08 05:07:40 -0500

    add radix ui and customize the theme

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 
 const Issues = () => {
   return (
     <div>
-      Issues
+        <Button >
+            <Link href='/issues/new'>New Issue</Link>
+        </Button>
     </div>
   )
 }
