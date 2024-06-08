e00d32c5a599d6bfa97fab078011ad8811d10220
Galaxyz <97992906+Galaxyz7@users.noreply.github.com>
2024-06-08 02:08:30 -0500

    add prisma sql handling'

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
-import { FaBug } from "react-icons/fa";
+import { FaBug } from "react-icons/fa"
+import classnames from 'classnames'
 
 const NavBar = () => {
-const links = [
-    {label: 'Dashboard', href: '/'},
-    {label: 'Issues', href: '/issues'},
-]
+    const currentPath = usePathname()
+    const links = [
+        {label: 'Dashboard', href: '/'},
+        {label: 'Issues', href: '/issues'},
+    ]
 
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
@@ -15,7 +20,11 @@ const links = [
         {links.map(link => 
             <Link 
                 key={link.href} 
-                className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                className={classnames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true,
+                }) }
                 href={link.href}>{link.label}
             </Link>)
         }
